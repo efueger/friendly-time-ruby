4b04faabf3c92501260a497a45bb31ceab67eef7
bellabling <barry.drinkwater@gmail.com>
2016-11-28 10:06:56 +0000

    handle future times plus tests

--- lib/friendly_time/friendly_time_provider.rb
+++ lib/friendly_time/friendly_time_provider.rb
@@ -6,61 +6,155 @@ module FriendlyTime
 	@one_week_in_seconds = @one_day_in_seconds * 7
 	@one_month_in_seconds = @one_day_in_seconds * 28
 	@one_year_in_seconds = @one_month_in_seconds * 12
+	
 	def self.friendly_time(now_time, other_time)
 		timeInSecs = now_time - other_time
-		if timeInSecs < @one_minute_in_seconds
-			"seconds ago"
-		elsif timeInSecs < @one_minute_in_seconds * 2
-			"about a minute ago"
-		elsif timeInSecs < @one_minute_in_seconds * 10
-			"minutes ago"
-		elsif timeInSecs < @one_minute_in_seconds * 15
-			"about 10 minutes ago"
-		elsif timeInSecs < @one_minute_in_seconds * 20
-			"about 15 minutes ago"
-		elsif timeInSecs < @one_minute_in_seconds * 25
-			"about 20 minutes ago"
-		elsif timeInSecs < @one_minute_in_seconds * 30
-			"about 25 minutes ago"
-		elsif timeInSecs < @one_minute_in_seconds * 45
-			"about half an hour ago"
-		elsif timeInSecs < @one_hour_in_seconds
-			"about 45 minutes ago"
-		elsif timeInSecs < @one_hour_in_seconds * 2
-			"about an hour ago"
-		elsif timeInSecs < @one_hour_in_seconds * 3
-			"a couple of hours ago"
-		elsif timeInSecs < @one_hour_in_seconds * 12
-			"a few hours ago"
-		elsif timeInSecs < @one_hour_in_seconds * 18
-			"about 12 hours ago"
-		elsif timeInSecs < @one_day_in_seconds
-			"about 18 hours ago"
-		elsif timeInSecs < @one_day_in_seconds * 2
-			"yesterday"
-		elsif timeInSecs < @one_day_in_seconds * 3
-			"a couple of days ago"
-		elsif timeInSecs < @one_week_in_seconds
-			"days ago"
-		elsif timeInSecs < @one_week_in_seconds * 2
-			"about a week ago"
-		elsif timeInSecs < @one_week_in_seconds * 3
-			"a couple of weeks ago"
-		elsif timeInSecs < @one_month_in_seconds * 2
-			"weeks ago"
-		elsif timeInSecs < @one_year_in_seconds
-			"months ago"
-		elsif timeInSecs < @one_year_in_seconds * 2
-			"about a year ago"
-		elsif timeInSecs < @one_year_in_seconds * 3
-			"a couple of years ago"
+		
+		if timeInSecs.abs < @one_minute_in_seconds
+			if timeInSecs > 0
+				"seconds ago"
+			else
+				"in less than a minute"
+			end
+		elsif timeInSecs.abs < @one_minute_in_seconds * 2
+			if timeInSecs > 0
+				"about a minute ago"
+			else
+				"in about a minute"
+			end
+		elsif timeInSecs.abs < @one_minute_in_seconds * 10
+			if timeInSecs > 0
+				"minutes ago"
+			else
+				"in a few minutes"
+			end
+		elsif timeInSecs.abs < @one_minute_in_seconds * 15
+			if timeInSecs > 0
+				"about 10 minutes ago"
+			else
+				"in about 10 minutes"
+			end
+		elsif timeInSecs.abs < @one_minute_in_seconds * 20
+			if timeInSecs > 0
+				"about 15 minutes ago"
+			else
+				"in about 15 minutes"
+			end
+		elsif timeInSecs.abs < @one_minute_in_seconds * 25
+			if timeInSecs > 0
+				"about 20 minutes ago"
+			else
+				"in about 20 minutes"
+			end
+		elsif timeInSecs.abs < @one_minute_in_seconds * 30
+			if timeInSecs > 0
+				"about 25 minutes ago"
+			else
+				"in about 25 minutes"
+			end
+		elsif timeInSecs.abs < @one_minute_in_seconds * 45
+			if timeInSecs > 0
+				"about half an hour ago"
+			else
+				"in about half an hour"
+			end
+		elsif timeInSecs.abs < @one_hour_in_seconds
+			if timeInSecs > 0
+				"about 45 minutes ago"
+			else
+				"in about 45 minutes"
+			end
+		elsif timeInSecs.abs < @one_hour_in_seconds * 2
+			if timeInSecs > 0
+				"about an hour ago"
+			else
+				"in about an hour"
+			end
+		elsif timeInSecs.abs < @one_hour_in_seconds * 3
+			if timeInSecs > 0
+				"a couple of hours ago"
+			else
+				"in a couple of hours"
+			end
+		elsif timeInSecs.abs < @one_hour_in_seconds * 12
+			if timeInSecs > 0
+				"a few hours ago"
+			else
+				"in a few hours"
+			end
+		elsif timeInSecs.abs < @one_hour_in_seconds * 18
+			if timeInSecs > 0
+				"about 12 hours ago"
+			else
+				"in about 12 hours"
+			end
+		elsif timeInSecs.abs < @one_day_in_seconds
+			if timeInSecs > 0
+				"about 18 hours ago"
+			else
+				"in about 18 hours"
+			end
+		elsif timeInSecs.abs < @one_day_in_seconds * 2
+			
+			if timeInSecs > 0
+				"yesterday"
+			else
+				"tomorrow"
+			end
+		elsif timeInSecs.abs < @one_day_in_seconds * 3
+			if timeInSecs > 0
+				"a couple of days ago"
+			else
+				"in a couple of days"
+			end
+			
+		elsif timeInSecs.abs < @one_week_in_seconds
+			if timeInSecs > 0
+				"days ago"
+			else
+				"in a few days"
+			end
+		elsif timeInSecs.abs < @one_week_in_seconds * 2
+			if timeInSecs > 0
+				"about a week ago"
+			else
+				"in about a week"
+			end
+		elsif timeInSecs.abs < @one_week_in_seconds * 3
+			if timeInSecs > 0
+				"a couple of weeks ago"
+			else
+				"in a couple of weeks"
+			end
+		elsif timeInSecs.abs < @one_month_in_seconds * 2
+			if timeInSecs > 0
+				"weeks ago"
+			else
+				"in a few weeks"
+			end
+		elsif timeInSecs.abs < @one_year_in_seconds
+			if timeInSecs > 0
+				"months ago"
+			else
+				"in a few months"
+			end
+		elsif timeInSecs.abs < @one_year_in_seconds * 2
+			if timeInSecs > 0
+				"about a year ago"
+			else
+				"in about a year"
+			end
+		elsif timeInSecs.abs < @one_year_in_seconds * 3
+			if timeInSecs > 0
+				"a couple of years ago"
+			else
+				"in a couple of years"
+			end
 		elsif timeInSecs > 0
 			"years ago"
+		else
+			"years from now"
 		end
-		
-		
-		
-		
 	end
   end
 end
--- spec/friendly_time_spec.rb
+++ spec/friendly_time_spec.rb
@@ -4,104 +4,222 @@ describe FriendlyTime do
     it 'has a version number' do
     expect(FriendlyTime::VERSION).not_to be nil
 	end
-
+	
+	before(:all) do
+		@one_minute_in_seconds = 60
+		@one_hour_in_seconds = @one_minute_in_seconds * 60
+		@one_day_in_seconds = @one_hour_in_seconds * 24
+		@one_week_in_seconds = @one_day_in_seconds * 7
+		@one_month_in_seconds = @one_day_in_seconds * 28
+		@one_year_in_seconds = @one_month_in_seconds * 12
+	end
+	
 	it 'answers with seconds ago' do
 		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(5, 0)).to eql("seconds ago")
 	end
 
 	it 'answers with about a minute ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(60, 0)).to eql("about a minute ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_minute_in_seconds, 0)).to eql("about a minute ago")
 	end
 
 	it 'answers with minutes ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(120, 0)).to eql("minutes ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_minute_in_seconds * 2, 0)).to eql("minutes ago")
 	end
 
 	it 'answers with minutes ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(300, 0)).to eql("minutes ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_minute_in_seconds * 5, 0)).to eql("minutes ago")
 	end
 
 	it 'answers with about 10 minutes ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(600, 0)).to eql("about 10 minutes ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_minute_in_seconds * 10, 0)).to eql("about 10 minutes ago")
 	end
 
 	it 'answers with about 15 minutes ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(900, 0)).to eql("about 15 minutes ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_minute_in_seconds * 15, 0)).to eql("about 15 minutes ago")
 	end
 
 	it 'answers with about 20 minutes ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(1200, 0)).to eql("about 20 minutes ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_minute_in_seconds * 20, 0)).to eql("about 20 minutes ago")
 	end
 
 	it 'answers with about 25 minutes ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(1500, 0)).to eql("about 25 minutes ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_minute_in_seconds * 25, 0)).to eql("about 25 minutes ago")
 	end
 
 	it 'answers with about half an hour ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(1800, 0)).to eql("about half an hour ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_minute_in_seconds * 30, 0)).to eql("about half an hour ago")
 	end
 
 	it 'answers with about 45 minutes ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(2700, 0)).to eql("about 45 minutes ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_minute_in_seconds * 45, 0)).to eql("about 45 minutes ago")
 	end
 
 	it 'answers with about an hour ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(3600, 0)).to eql("about an hour ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_hour_in_seconds, 0)).to eql("about an hour ago")
 	end
 
 	it 'answers with a couple of hours ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(7200, 0)).to eql("a couple of hours ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_hour_in_seconds * 2, 0)).to eql("a couple of hours ago")
 	end
 
 	it 'answers with a few hours ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(14400, 0)).to eql("a few hours ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_hour_in_seconds * 3, 0)).to eql("a few hours ago")
 	end
 
 	it 'answers with about 12 hours ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(43200, 0)).to eql("about 12 hours ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_hour_in_seconds * 12, 0)).to eql("about 12 hours ago")
 	end
 
 	it 'answers with about 18 hours ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(64800, 0)).to eql("about 18 hours ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_hour_in_seconds * 18, 0)).to eql("about 18 hours ago")
 	end
 
 	it 'answers with yesterday' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(86400, 0)).to eql("yesterday")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_day_in_seconds, 0)).to eql("yesterday")
 	end
 
 	it 'answers with couple of days ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(172800, 0)).to eql("a couple of days ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_day_in_seconds * 2, 0)).to eql("a couple of days ago")
 	end
 
 	it 'answers with days ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(259200, 0)).to eql("days ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_day_in_seconds * 3, 0)).to eql("days ago")
 	end
 
 	it 'answers with about a week ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(604800, 0)).to eql("about a week ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_week_in_seconds, 0)).to eql("about a week ago")
 	end
 
 	it 'answers with a couple of weeks ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(1209600, 0)).to eql("a couple of weeks ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_week_in_seconds * 2, 0)).to eql("a couple of weeks ago")
 	end
 
 	it 'answers with weeks ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(1814400, 0)).to eql("weeks ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_week_in_seconds * 3, 0)).to eql("weeks ago")
 	end
 
 	it 'answers with months ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(4838400, 0)).to eql("months ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_month_in_seconds * 2, 0)).to eql("months ago")
 	end
 
 	it 'answers with about a year ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(31449600, 0)).to eql("about a year ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_year_in_seconds, 0)).to eql("about a year ago")
 	end
 
 	it 'answers with a couple of years ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(62899200, 0)).to eql("a couple of years ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_year_in_seconds * 2, 0)).to eql("a couple of years ago")
 	end
 
 	it 'answers with years ago' do
-		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(94348800, 0)).to eql("years ago")
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(@one_year_in_seconds * 3, 0)).to eql("years ago")
+	end
+	
+	#########################################################
+	
+	it 'answers with in less than a minute' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, 5)).to eql("in less than a minute")
+	end	
+	
+	it 'answers with in about a minute' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_minute_in_seconds)).to eql("in about a minute")
+	end
+	
+	it 'answers with in a few minutes' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_minute_in_seconds * 2)).to eql("in a few minutes")
+	end
+	
+	
+	
+	
+	
+	
+	
+	
+	it 'answers with in a few minutes' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_minute_in_seconds * 5)).to eql("in a few minutes")
+	end
+
+	it 'answers with in about 10 minutes' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_minute_in_seconds * 10)).to eql("in about 10 minutes")
+	end
+
+	it 'answers with in about 15 minutes' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_minute_in_seconds * 15)).to eql("in about 15 minutes")
+	end
+
+	it 'answers with in about 20 minutes' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_minute_in_seconds * 20)).to eql("in about 20 minutes")
+	end
+
+	it 'answers with in about 25 minutes' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_minute_in_seconds * 25)).to eql("in about 25 minutes")
+	end
+
+	it 'answers with in about half an hour' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_minute_in_seconds * 30)).to eql("in about half an hour")
+	end
+
+	it 'answers with in about 45 minutes' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_minute_in_seconds * 45)).to eql("in about 45 minutes")
+	end
+
+	it 'answers with in about an hour' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_hour_in_seconds)).to eql("in about an hour")
+	end
+
+	it 'answers with in a couple of hours' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_hour_in_seconds * 2)).to eql("in a couple of hours")
+	end
+
+	it 'answers with in a few hours' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_hour_in_seconds * 3)).to eql("in a few hours")
+	end
+
+	it 'answers with in about 12 hours' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_hour_in_seconds * 12)).to eql("in about 12 hours")
+	end
+
+	it 'answers with in about 18 hours' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_hour_in_seconds * 18)).to eql("in about 18 hours")
+	end
+
+	it 'answers with tomorrow' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_day_in_seconds)).to eql("tomorrow")
+	end
+
+	it 'answers with in couple of days' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_day_in_seconds * 2)).to eql("in a couple of days")
+	end
+
+	it 'answers with in a few days' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_day_in_seconds * 3)).to eql("in a few days")
+	end
+
+	it 'answers with in about a week' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_week_in_seconds)).to eql("in about a week")
+	end
+
+	it 'answers with in a couple of weeks' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_week_in_seconds * 2)).to eql("in a couple of weeks")
+	end
+
+	it 'answers with in a few weeks' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_week_in_seconds * 3)).to eql("in a few weeks")
+	end
+
+	it 'answers with in a few months' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_month_in_seconds * 2)).to eql("in a few months")
+	end
+
+	it 'answers with in about a year' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_year_in_seconds)).to eql("in about a year")
+	end
+
+	it 'answers with in a couple of years' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_year_in_seconds * 2)).to eql("in a couple of years")
+	end
+
+	it 'answers with years from now' do
+		expect(FriendlyTime::FriendlyTimeProvider.friendly_time(0, @one_year_in_seconds * 3)).to eql("years from now")
 	end
 end
