f8f2b632c2b92e34d3b7157377c24e9a5f27d7ff
bellabling <barry.drinkwater@gmail.com>
2016-12-02 16:14:41 +0000

    change name of 'task'

--- lib/friendly_time/cli.rb
+++ lib/friendly_time/cli.rb
@@ -2,8 +2,8 @@ require 'thor'
 require 'friendly_time'
 module FriendlyTime
   class CLI < Thor
-	desc "friendly_time other_time now_time", "Returns a casual description of the time between other_time & now_time. now_time is optional - if not provided, the current time will be used."
-	def friendly_time(other_time, now_time = (Time.now.to_i * 1000))
+	desc "print_friendly_time other_time now_time", "Prints a casual description of the time between other_time & now_time. now_time is optional - if not provided, the current time will be used."
+	def print_friendly_time(other_time, now_time = (Time.now.to_i * 1000))
 	  puts FriendlyTime::FriendlyTimeProvider.friendly_time(now_time.to_i, other_time.to_i)
 	end
   end
