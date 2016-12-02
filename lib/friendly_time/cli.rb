034bf08490fbc1f2e3fb4074da203a4f577dc835
bellabling <barry.drinkwater@gmail.com>
2016-12-02 15:21:44 +0000

    print result

--- lib/friendly_time/cli.rb
+++ lib/friendly_time/cli.rb
@@ -4,7 +4,7 @@ module FriendlyTime
   class CLI < Thor
 	desc "friendly_time other_time now_time", "Returns a casual description of the time between other_time & now_time. now_time is optional - if not provided, the current time will be used."
 	def friendly_time(other_time, now_time = (Time.now.to_i * 1000))
-	  FriendlyTime::FriendlyTimeProvider.friendly_time(now_time.to_i, other_time.to_i)
+	  puts FriendlyTime::FriendlyTimeProvider.friendly_time(now_time.to_i, other_time.to_i)
 	end
   end
 end
