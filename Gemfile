000f9f6d5b4e46799436eb10cf40cd04a6518760
bellabling <barry.drinkwater@gmail.com>
2016-12-05 09:36:56 +0000

    remove all but code and tests

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-# Specify your gem's dependencies in friendly_time.gemspec
-gemspec
--- lib/friendly_time.rb
+++
@@ -1,6 +0,0 @@
-require "friendly_time/version"
-require "friendly_time/friendly_time_provider"
-
-module FriendlyTime
-  # Your code goes here...
-end
